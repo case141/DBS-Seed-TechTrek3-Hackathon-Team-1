b5281569cd741e728f91ea41a4319b0b39a44afe
Nwx, Cheryl <cheryl.nwx@accenture.com>
2021-03-05 12:32:02 +0800

    Display Current Pricing

---
+++ techtrek-app/src/components/CurrentPrice/CurrentPricing.js
@@ -0,0 +1,53 @@
+
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import {Card } from 'react-bootstrap'
+import './index.css'
+function CurrentPricing() {
+  const [currentPrice, setCurrentPrice] = useState({})
+
+  useEffect(() => {
+    let data = axios.post(
+      'https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/pricing/current',
+      {},
+      {
+        headers: {
+          'x-api-key': 'dgkCTGTaXm7HYZNgyizLY4ocEVSO7G3c54QcYSIu'
+        }
+      }
+    )
+    .then ( function (res) {
+      setCurrentPrice(res.data)
+    })
+    setInterval(() => {
+      data = axios.post(
+        'https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/pricing/current',
+        {},
+        {
+          headers: {
+            'x-api-key': 'dgkCTGTaXm7HYZNgyizLY4ocEVSO7G3c54QcYSIu'
+          }
+        }
+      )
+      .then (function (res) {
+        setCurrentPrice(res.data)
+      })
+
+
+    }, (60 * 1000))
+  }, [])
+
+  return (
+    <Card style={{ width: '18rem' }}>
+  <Card.Body>
+    <Card.Title>{`$${currentPrice.price}`}</Card.Title>
+    <Card.Text>
+      {currentPrice.assetSymbol}
+    </Card.Text>
+  </Card.Body>
+</Card>
+      
+  )
+}
+
+export default CurrentPricing
