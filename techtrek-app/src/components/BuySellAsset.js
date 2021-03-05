112642e90edbe84f8e3c57d62d48c7c187b42b57
Viriyadhika Putra <viriya.dhika0@gmail.com>
2021-03-05 12:37:08 +0800

    get validation completed

--- techtrek-app/src/components/BuySellAsset.js
+++ techtrek-app/src/components/BuySellAsset.js
@@ -4,7 +4,7 @@ import { Form, Button } from 'react-bootstrap';
 import axios from 'axios';
 
 export function BuySellAsset() {
-  const [orderType, setOrderType] = useState('');
+  const [orderType, setOrderType] = useState('BUY');
   const [assetAmount, setAssetAmount] = useState(0);
 
   function handleTypeChange(e) {
@@ -16,48 +16,58 @@ export function BuySellAsset() {
 
   function handleAmountChange(e) {
     e.preventDefault();
-    setAssetAmount(e.target.value)
+    setAssetAmount(e.target.value);
+    if (isNaN(e.target.value)) {
+      console.log('not a number');
+    }
   }
 
 
   async function callAPI() {
-    let data = await axios.post(
-      'https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/transactions/add',
-      {
-        accountKey: 'b60212f0-46fb-41fb-aa38-2d609a36a419',
-        orderType,
-        assetAmount
-      },
-      {
-        headers: {
-          'x-api-key':  'dgkCTGTaXm7HYZNgyizLY4ocEVSO7G3c54QcYSIu'
+    try {
+      let data = await axios.post(
+        'https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/transactions/add',
+        {
+          accountKey: 'b60212f0-46fb-41fb-aa38-2d609a36a419',
+          orderType,
+          assetAmount
+        },
+        {
+          headers: {
+            'x-api-key':  'dgkCTGTaXm7HYZNgyizLY4ocEVSO7G3c54QcYSIu'
+          }
         }
-      }
-    )
-
-    console.log(data);
-
+      )
+      console.log(data);
+      alert('transaction successful ' + data.data.orderType + ' ' + data.data.assetAmount + ' ' + data.data.assetSymbol)
+    } catch (error) {
+      console.log(error);
+      alert('Transaction has failed. ' + error.body);
+    }
   }
 
 
   function handleSubmit(e) {
     e.preventDefault();
-    callAPI();
-    console.log(orderType, assetAmount);
+    if (e.currentTarget.checkValidity() !== false) {
+      callAPI();
+    }
   }
 
 
   return (
-    <div>
-      <Form onSubmit={handleSubmit}>
+    <div style={{margin:'20px'}}>
+      <Form validated={false} onSubmit={handleSubmit}>
         <Form.Group controlId='transaction-type'>
           <Form.Label>Transaction Type</Form.Label>
-          <Form.Control onChange={handleTypeChange} value={orderType} type='text' placeholder="Enter type">
+          <Form.Control as='select' onChange={handleTypeChange} value={orderType} type='text' placeholder="Enter type">
+            <option value="BUY">Buy</option>
+            <option value="SELL">Sell</option>
           </Form.Control>
         </Form.Group>
         <Form.Group controlId='asset-amount'>
           <Form.Label>Asset Amount</Form.Label>
-          <Form.Control onChange={handleAmountChange} value={assetAmount} type='float' placeholder="Enter type"/>
+          <Form.Control onChange={handleAmountChange} value={assetAmount} type='number' step='0.01' placeholder="Enter amount"/>
         </Form.Group>
         <Button variant="primary" type="submit">Submit Transaction</Button>
       </Form>
